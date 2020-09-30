160bcfc8634254652fc750c12fa97a4edc2dda7d
mindforrest <tfseverson@gmail.com>
2020-09-30 01:44:32 -0700

    removed unused libraries and functions for space

--- keyboards/lily58/keymaps/spoodR/keymap.c
+++ keyboards/lily58/keymaps/spoodR/keymap.c
@@ -38,17 +38,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #ifdef OLED_DRIVER_ENABLE
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_keyboard_master())
-    return OLED_ROTATION_0;  // flips the display 180 degrees if offhand
-  return rotation;
+    return OLED_ROTATION_0;  // flips the display, prefer 0-180 for processing
 }
 
 // When you add source files to SRC in rules.mk, you can use functions.
-const char *read_layer_state(void);
-const char *read_logo(void);
-void set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keylog(void);
-const char *read_keylogs(void);
+//const char *read_layer_state(void);
+//const char *read_logo(void);
+//void set_keylog(uint16_t keycode, keyrecord_t *record);
+//const char *read_keylog(void);
+//const char *read_keylogs(void);
 
 // const char *read_mode_icon(bool swap);
 // const char *read_host_led_state(void);
@@ -94,30 +92,17 @@ static void draw_icon(void) {
 }
 
 void oled_task_user(void) {
-  if (is_keyboard_master()) {
-    // If you want to change the display of OLED, you need to change here
-    oled_write_ln(read_layer_state(), false);
-    oled_write_ln(read_keylog(), false);
-    oled_write_ln(read_keylogs(), false);
-    //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
-    //oled_write_ln(read_host_led_state(), false);
-    //oled_write_ln(read_timelog(), false);
-  } else {
-//    oled_write(read_logo(), false);
 	draw_icon();
   }
-}
+
 
 
 
 #endif // OLED_DRIVER_ENABLE
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-#ifdef OLED_DRIVER_ENABLE
-    set_keylog(keycode, record);
-#endif
-    // set_timelog();
+
+
     switch (keycode) {
 
   case HEART:
@@ -134,7 +119,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
         break;
 
-  }
   }
   return true;
 }
