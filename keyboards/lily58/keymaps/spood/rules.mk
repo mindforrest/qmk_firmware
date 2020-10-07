ca6933f56e3f94320bb1c7243ee3960d99d0699c
mindforrest <tfseverson@gmail.com>
2020-10-06 19:05:49 -0700

    layout and macro changes. turned dynamic macros off

--- keyboards/lily58/keymaps/spood/rules.mk
+++ keyboards/lily58/keymaps/spood/rules.mk
@@ -17,14 +17,14 @@ RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_DRIVER_ENABLE= yes     # OLED display
 KEY_LOCK_ENABLE = yes
-DYNAMIC_MACRO_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = no
 DEBOUNCE_TYPE = sym_defer_pk
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
 # If you want to change the display of OLED, you need to change here
-SRC +=   ./lib/rgb_state_reader.c \
+SRC +=  ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
         ./lib/logo_reader.c \
         ./lib/keylogger.c \
